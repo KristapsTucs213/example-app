65a166daf3a53ee5d84c81848f5d4ef4ee8bd595
unknown <you@example.com>
2024-10-17 14:16:57 +0300

    this is a commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        $posts = Post::all();
+        return view("posts.index", ['allPosts' => $posts]);
+    }
     //
+
+    public function show($id)
+    {
+        $post = Post::Find($id);
+        // \Log::debug($id);
+        return view('posts.show' , ['post -> $post']);
+    }
 }
